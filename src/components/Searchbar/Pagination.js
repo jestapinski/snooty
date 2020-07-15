d0e4f5e791160f21b0faa9a6a734cf8890c91b06
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-07-15 17:18:34 -0400

    Add basic pagination component

---
+++ src/components/Searchbar/Pagination.js
@@ -0,0 +1,83 @@
+import React, { useCallback, useMemo } from 'react';
+import styled from '@emotion/styled';
+import Button from '@leafygreen-ui/button';
+import Icon from '@leafygreen-ui/icon';
+import { theme } from '../../theme/docsTheme';
+
+const BUTTON_SIZE = theme.size.medium;
+
+const PaginationButton = styled(Button)`
+  background-color: #fff;
+  border-radius: ${BUTTON_SIZE};
+  height: ${BUTTON_SIZE};
+  /* button is 24 px and entire container is 36px so 6px top gives equal spacing */
+
+  width: ${BUTTON_SIZE};
+  z-index: 1;
+  /* Below removes default hover effects from button */
+  background-image: none;
+  border: none;
+  box-shadow: none;
+  :before {
+    display: none;
+  }
+  :after {
+    display: none;
+  }
+`;
+
+const PaginationButtonIcon = styled(Icon)`
+  /* This icon is 16px tall in a 24 px button, so 4px gives equal spacing */
+  left: ${theme.size.tiny};
+  position: absolute;
+  top: ${theme.size.tiny};
+`;
+
+const PaginationContainer = styled('div')`
+  display: flex;
+`;
+
+const PaginationText = styled('p')`
+  margin-bottom: 0;
+  line-height: 22px;
+`;
+
+const Pagination = ({ currentPage, setCurrentPage, totalPages }) => {
+  const decrementPage = useCallback(() => setCurrentPage(currentPage - 1), [currentPage, setCurrentPage]);
+  const incrementPage = useCallback(() => setCurrentPage(currentPage + 1), [currentPage, setCurrentPage]);
+  const canDecrementPage = useMemo(() => currentPage !== 1, [currentPage]);
+  const canIncrementPage = useMemo(() => currentPage < totalPages, [currentPage, totalPages]);
+  return (
+    <PaginationContainer>
+      <PaginationButton
+        aria-label="Back Page"
+        disabled={!canDecrementPage}
+        glyph={
+          <PaginationButtonIcon
+            glyph="ChevronLeft"
+            fill={canDecrementPage ? '#3D4F58' : '#B8C4C2'}
+            onClick={decrementPage}
+          />
+        }
+      />
+      <PaginationText>
+        <strong>
+          {currentPage}/{totalPages}
+        </strong>
+      </PaginationText>
+      <PaginationButton
+        aria-label="Forward Page"
+        disabled={!canIncrementPage}
+        glyph={
+          <PaginationButtonIcon
+            glyph="ChevronRight"
+            fill={canIncrementPage ? '#3D4F58' : '#B8C4C2'}
+            onClick={incrementPage}
+          />
+        }
+      />
+    </PaginationContainer>
+  );
+};
+
+export default Pagination;
