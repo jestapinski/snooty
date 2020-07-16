1b225350a0dbe4eec227c221ee9300a52ab790d7
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-07-16 11:49:12 -0400

    Display naive data on search results with pagination

---
+++ src/components/Searchbar/SearchResults.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { theme } from '../../theme/docsTheme';
+
+const SEARCHBAR_HEIGHT = 36;
+const SEARCH_RESULTS_DESKTOP_HEIGHT = 368;
+const SEARCH_FOOTER_DESKTOP_HEIGHT = theme.size.xlarge;
+
+const SearchResultContainer = styled('div')`
+  display: block;
+`;
+
+const SearchResultsContainer = styled('div')`
+  box-shadow: 0 0 ${theme.size.tiny} 0 rgba(184, 196, 194, 0.48);
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 1fr 1fr 1fr;
+  height: ${SEARCH_RESULTS_DESKTOP_HEIGHT}px;
+  position: relative;
+  /* Give top padding on desktop to offset this extending into the searchbar */
+  padding-top: ${theme.size.default};
+  width: 100%;
+  @media ${theme.screenSize.upToXSmall} {
+    box-shadow: none;
+    /* On mobile, let the dropdown take the available height */
+    height: calc(100% - ${SEARCH_FOOTER_DESKTOP_HEIGHT} - ${SEARCHBAR_HEIGHT}px);
+    padding-top: 0;
+  }
+`;
+
+const SearchResult = React.memo(({ preview, title, url }) => (
+  <SearchResultContainer>
+    <p>Title: {title}</p>
+    <p>Preview: {preview}</p>
+    <p>Url: {url}</p>
+  </SearchResultContainer>
+));
+
+const SearchResults = ({ visibleResults }) => (
+  <SearchResultsContainer>
+    {visibleResults.map(({ title, preview, url }) => (
+      <SearchResult title={title} preview={preview} url={url} />
+    ))}
+  </SearchResultsContainer>
+);
+
+export default SearchResults;
