9c86d0e286db86e5f735456b117c53b0e4d08cfc
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-07-24 10:23:16 -0400

    Add small fade-in

--- src/components/Searchbar/AdvancedFiltersPane.js
+++ src/components/Searchbar/AdvancedFiltersPane.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { keyframes } from '@emotion/core';
 import styled from '@emotion/styled';
 import Button from '@leafygreen-ui/button';
 import Icon from '@leafygreen-ui/icon';
 import { uiColors } from '@leafygreen-ui/palette';
 import { theme } from '../../theme/docsTheme';
 
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
 const SEARCH_RESULTS_DESKTOP_HEIGHT = '368px';
+
+const StyledContentContainer = styled('div')`
+  animation: ${fadeIn} 300ms ease;
+`;
 
 const StyledAdvancedFiltersPane = styled('div')`
   box-shadow: 0 0 ${theme.size.tiny} 0 rgba(184, 196, 194, 0.48);
@@ -38,10 +53,12 @@ const StyledReturnButton = styled(Button)`
 const AdvancedFiltersPane = ({ closeFiltersPane }) => {
   return (
     <StyledAdvancedFiltersPane>
-      <StyledReturnButton onClick={closeFiltersPane}>
-        <Icon glyph="ArrowLeft" size="small" />
-        &nbsp;Cancel
-      </StyledReturnButton>
+      <StyledContentContainer>
+        <StyledReturnButton onClick={closeFiltersPane}>
+          <Icon glyph="ArrowLeft" size="small" />
+          &nbsp;Cancel
+        </StyledReturnButton>
+      </StyledContentContainer>
     </StyledAdvancedFiltersPane>
   );
 };
